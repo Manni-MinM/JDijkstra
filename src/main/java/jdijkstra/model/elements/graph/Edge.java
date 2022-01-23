60b1ef9039f0be7ade6ed616ed4a1029a2db325f
manni-minm <mannimoghimi@gmail.com>
2022-01-24 03:06:12 +0330

    feat: added graph components

---
+++ src/main/java/jdijkstra/model/elements/graph/Edge.java
@@ -0,0 +1,27 @@
+// BWOTSHEWCHB
+
+package jdijkstra.model.elements.graph;
+
+public class Edge {
+	// Fields
+	private Node from;
+	private Node to;
+	private int weight;
+	// Constructor
+	public Edge(Node from, Node to, int weight) {
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+	// Getters
+	public Node getFrom() {
+		return this.from;
+	}
+	public Node getTo() {
+		return this.to;
+	}
+	public int getWeight() {
+		return this.weight;
+	}
+}
+
---
+++ src/main/java/jdijkstra/model/elements/graph/Node.java
@@ -0,0 +1,17 @@
+// BWOTSHEWCHB
+
+package jdijkstra.model.elements.graph;
+
+public class Node {
+	// Fields
+	private int id;
+	// Constructor
+	public Node(int id) {
+		this.id = id;
+	}	
+	// Getters
+	public int getId() {
+		return this.id;
+	}
+}
+
