1680e1a10eb60f05fe0541da523016d166800d3b
manni-minm <mannimoghimi@gmail.com>
2022-01-24 14:56:41 +0330

    feat: connected app to main controller

--- src/main/java/jdijkstra/App.java
+++ src/main/java/jdijkstra/App.java
@@ -1,25 +1,13 @@
 package jdijkstra;
 
-import jdijkstra.model.elements.map.*;
-import jdijkstra.model.elements.list.*;
-import jdijkstra.model.elements.graph.*;
+import jdijkstra.controller.MainController;
 
 public class App {
+	// Fields
+	private static MainController controller;
+	// Methods
 	public static void main(String[] args) {
-		// Sample testcase
-		Graph graph = new Graph();
-
-		graph.newNode(10);
-		graph.newNode(7);
-		graph.newNode(19);
-		graph.newNode(20);
-		graph.newNode(1);
-
-		graph.newEdge(10, 7, 50);
-		graph.newEdge(19, 7, 20);
-		graph.newEdge(7, 20, 1);
-		graph.newEdge(1, 20, 5);
-
-		System.out.println(graph.toString());
+		controller = new MainController();
+		controller.mainLoop();
 	}
 }
