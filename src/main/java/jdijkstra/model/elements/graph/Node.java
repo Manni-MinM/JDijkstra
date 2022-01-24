59a11153a2cc7bb1e22e993edd1151136862e085
manni-minm <mannimoghimi@gmail.com>
2022-01-24 03:48:13 +0330

    feat: connected list to graph nodes

--- src/main/java/jdijkstra/model/elements/graph/Node.java
+++ src/main/java/jdijkstra/model/elements/graph/Node.java
@@ -2,16 +2,27 @@
 
 package jdijkstra.model.elements.graph;
 
+import jdijkstra.model.elements.list.*;
+
 public class Node {
 	// Fields
 	private int id;
+	private List<Edge> adjList;
 	// Constructor
 	public Node(int id) {
 		this.id = id;
+		this.adjList = new List<Edge>();
 	}	
 	// Getters
 	public int getId() {
 		return this.id;
+	}
+	public List<Edge> getAdjList() {
+		return this.adjList;
+	}
+	// Methods
+	void addAdjNode(Node to, int weight) {
+		adjList.append(new Edge(this, to, weight));
 	}
 }
 
